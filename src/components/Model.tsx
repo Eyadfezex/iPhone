07aced5fdf8669db81da284338e413cf798234fe
Eyad ahmed <Eyad932004@outloo.com>
2024-06-09 23:46:33 +0300

    start model component

---
+++ src/components/Model.tsx
@@ -0,0 +1,13 @@
+const Model = () => {
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 className="section-heading" id="heading">
+          Take a closer look.
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
